132e46c3675eb59263f50e7839edabff7f6a508d
Thiago Costa <thiagodias2708@gmail.com>
2022-02-17 23:23:43 -0300

    Initial Commit

---
+++ Shared/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Shared
+//
+//  Created by Thiago Costa on 17/02/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
